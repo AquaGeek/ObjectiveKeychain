381dbd0b67c4e4566d079b7e3025909600ed0869
Tyler Stromberg <tylers@mac.com>
2012-10-19 17:18:12 -0400

    Update copyright statement

--- Library/OKKey.h
+++ Library/OKKey.h
@@ -2,7 +2,7 @@
 //  OKKey.h
 //  ObjectiveKeychain
 //
-//  Copyright (c) 2010 Tyler Stromberg
+//  Copyright (c) 2010-2012 Tyler Stromberg
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
